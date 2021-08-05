61230696b5302bcedad442f6c4f19eb90818ee93
Nikolay Gagara <ggn.f763@gmail.com>
2021-08-05 19:23:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Button, Container } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { questions } from './data'
+
+const useStyles = makeStyles({
+  root: {
+    borderRadius: 3,
+    marginBottom: '10px',
+  }
+});
 
 function App() {
+
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const [userAnswers, setUserAnswers] = React.useState([]);
+  const [score, setScore] = React.useState(0);
+
+  const classes = useStyles();
+
+  const currentQuizDate = questions[currentQuestion];
+  
+  const handleClickAnswer = (answer, correctAnswer) => {
+    setCurrentQuestion(prev => ++prev)
+    if (answer === correctAnswer) {
+      setScore(prev => ++prev)
+      setUserAnswers(prev => [...prev, answer])
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <pre>{currentQuizDate.question}</pre>
+       { 
+         Object.entries(currentQuizDate.options).map(([answer, value]) => (
+          <Button key={answer} 
+          classes={{root: classes.root}} 
+          onClick={()=>handleClickAnswer(answer, currentQuizDate.correctAnswer)} 
+          variant="outlined" fullWidth={true} 
+          size='large'>{value}</Button>
+         ))
+       }
+      <h3>{currentQuestion+1} / {questions.length}</h3>
+      <h3>Правильных ответов {score}</h3>
+    </Container>
   );
 }
 
